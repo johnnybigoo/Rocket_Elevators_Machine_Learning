07d184a54321b62b4721e4afd256583bb2c2a0bb
johnnybigoo <jotalico@icloud.com>
2021-10-26 11:41:45 -0400

    redo-pages-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/residential'
+  get 'pages/commercial'
+  get 'pages/quote'
   root 'reocket_elevators#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
