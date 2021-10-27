6b629a46940dd320ebdcd6f719891cafd970cdfe
johnnybigoo <jotalico@icloud.com>
2021-10-27 14:26:45 -0400

    remake-pages-6

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'pages/residential'
   get 'pages/commercial'
   get 'pages/quote'
-  root 'reocket_elevators#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
