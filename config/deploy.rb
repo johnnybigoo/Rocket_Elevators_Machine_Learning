29b22b31c69ecbbde3fb09e2cf182ed2cf3bf49b
AlexWallot <alexwallot@outlook.com>
2021-10-28 16:27:52 -0400

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "JonesTavares"
 set :repo_url, "https://github.com/AlexWallot/Rocket_Elevators_Information_System"
 
-# Default branch is :master
+Default branch is :main
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
