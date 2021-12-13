4d5cd0faff0c33e6d8a4a6edf6b67ab0fa8c1f57
johnnybigoo <jotalico@icloud.com>
2021-12-12 21:05:55 -0500

    OWASP security review and adjustments in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 
-# Enable escaping HTML in JSON.
-config.active_support.escape_html_entities_in_json = true
 
 Bundler.require(*Rails.groups)
 
@@ -14,6 +12,9 @@ module RocketElevatorsInformationSystem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
